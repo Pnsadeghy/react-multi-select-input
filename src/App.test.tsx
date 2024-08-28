ead4c9acdd9488028997fce37dd801b66b5f4b8b
Pnsadeghy <pnsadeghy@gmail.com>
2024-08-28 17:17:25 +0330

    Feat: Updated App.test.tsx

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,9 @@
-import React from 'react';
 import { render, screen } from '@testing-library/react';
+import BaseDropdown from "./components/dropdown/BaseDropdown";
+import React from 'react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  expect(BaseDropdown).toBeInTheDocument();
 });
