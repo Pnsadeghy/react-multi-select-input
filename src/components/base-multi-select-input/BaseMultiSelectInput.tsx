20faea044c7d08408df66d7fef88eb74b51d4836
Pnsadeghy <pnsadeghy@gmail.com>
2024-08-29 11:13:26 +0330

    Feat: Added Hooks to BaseMultiSelectInput

--- src/components/base-multi-select-input/BaseMultiSelectInput.tsx
+++ src/components/base-multi-select-input/BaseMultiSelectInput.tsx
@@ -3,6 +3,8 @@ import BaseMultiSelectInputArrowIcon from "./BaseMultiSelectInputArrowIcon";
 import BaseMultiSelectInputItem from "./BaseMultiSelectInputItem";
 import React, {useState, useEffect, useRef, useMemo} from 'react';
 import "./BaseMultiSelectInput.scss"
+import useBaseMultiSelectInputHook from "./hooks/base.multi.select.input.hook";
+import useBaseMultiSelectInputOptionsHook from "./hooks/base.multi.select.input.options.hook";
 
 interface BaseMultiSelectInputProps {
     options: InputOptionInterface[];
@@ -15,12 +17,24 @@ const className = 'base-multi-select-input';
 
 const BaseMultiSelectInput: React.FC<BaseMultiSelectInputProps> = ({ options, model, placeholder, onChange }) => {
 
-    const wrapperRef = useRef<HTMLDivElement>(null);
-    const [isOpen, setIsOpen] = useState<boolean>(false);
-    const [isSearchFocus, setIsSearchFocus] = useState<boolean>(false);
-    const [searchValue, setSearchValue] = useState<string>("");
-    const [highlightIndex, setHighlightIndex] = useState<number>(-1);
-    const [internalOptions, setInternalOptions] = useState<InputOptionInterface[]>(options);
+    const {
+        wrapperRef,
+        isOpen,
+        setIsOpen
+    } = useBaseMultiSelectInputHook();
+
+    const {
+        searchValue,
+        setSearchValue,
+        isSearchFocus,
+        setIsSearchFocus,
+        highlightIndex,
+        setHighlightIndex,
+        internalOptions,
+        showOptions,
+        handleKeyDown,
+        toggleModelOptionsByValue
+    } = useBaseMultiSelectInputOptionsHook(options, model, onChange);
 
     const buttonText = useMemo(() => {
         if (model.length === 0) return placeholder || 'Select options...'
@@ -28,16 +42,6 @@ const BaseMultiSelectInput: React.FC<BaseMultiSelectInputProps> = ({ options, mo
             .filter((i: InputOptionInterface) => model.includes(i.value))
             .map((i: InputOptionInterface) => i.label).join(', ')
     }, [model, internalOptions]);
-
-    const showOptions = useMemo(() => {
-        const editedSearchValue = searchValue.toLowerCase();
-        return (editedSearchValue
-            ? internalOptions.filter((i: InputOptionInterface) => i.label.toLowerCase().includes(editedSearchValue))
-            : internalOptions).map((i: InputOptionInterface) => ({
-            ...i,
-            checked: model.includes(i.value)
-        }))
-    }, [searchValue, internalOptions, model]);
 
     const handleFocus = () => {
         setSearchValue("");
@@ -55,66 +59,6 @@ const BaseMultiSelectInput: React.FC<BaseMultiSelectInputProps> = ({ options, mo
         setSearchValue(event.target.value.trim());
         setHighlightIndex(-1);
     }
-
-    const handleKeyDown = (event: React.KeyboardEvent<HTMLInputElement>) => {
-        if (!isSearchFocus) return;
-
-        if (event.key === 'ArrowDown') {
-            if (highlightIndex < showOptions.length - 1)
-                setHighlightIndex(highlightIndex + 1);
-            return;
-        }
-
-        if (event.key === 'ArrowUp') {
-            if (highlightIndex > 0)
-                setHighlightIndex(highlightIndex - 1);
-            return;
-        }
-
-        if (event.key === 'Enter') {
-            const editedSearchValue = searchValue.toLowerCase();
-
-            if (highlightIndex < 0) {
-                if (!editedSearchValue) return;
-
-                const newOption: InputOptionInterface = { label: editedSearchValue, value: editedSearchValue };
-                setInternalOptions([...internalOptions, newOption]);
-                onChange([...model, newOption.value]);
-                setSearchValue("");
-                return;
-            }
-
-            toggleModelOptionsByValue(showOptions[highlightIndex].value);
-        }
-    };
-
-    const handleOptionClick = (option: InputOptionInterface) => {
-        toggleModelOptionsByValue(option.value);
-    }
-
-    const toggleModelOptionsByValue = (value: string) => {
-        const editableModel = [...model];
-        const index = editableModel.indexOf(value);
-        if (index >= 0)
-            editableModel.splice(index, 1);
-        else
-            editableModel.push(value);
-        onChange(editableModel);
-    }
-
-    useEffect(() => {
-        const handleClickOutside = (event: MouseEvent) => {
-            if (wrapperRef.current && !wrapperRef.current.contains(event.target as Node)) {
-                setIsOpen(false);
-            }
-        };
-
-        document.addEventListener('click', handleClickOutside);
-
-        return () => {
-            document.removeEventListener('click', handleClickOutside);
-        };
-    }, []);
 
     return (
         <div className={className} ref={wrapperRef}>
@@ -142,7 +86,7 @@ const BaseMultiSelectInput: React.FC<BaseMultiSelectInputProps> = ({ options, mo
                                 key={option.value}
                                 option={option}
                                 highlight={index === highlightIndex}
-                                onClick={() => handleOptionClick(option)} />
+                                onClick={() => toggleModelOptionsByValue(option.value)} />
                         ))
                     ) : (
                         <div className={`${className}_dropdown_empty`}>{searchValue ? 'Nothing found!' : 'Empty list'}</div>
